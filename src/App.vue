f1fd858115fcc203bd8a575cdf851f7393dcb5f8
dimas-lex <dimaslex@gmail.com>
2018-02-21 23:11:57 +0100

    Add code

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <AsyncComponent />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {
+    AsyncComponent: () => import('./AsyncComponent.vue'),
+  },
+};
+</script>
+
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    background-color: #213443;
+    color: #2c3e50;
+    margin-top: 0px;
+    padding: 0;
+    width: 100vw;
+    height: 100vh;
+  }
+</style>
---
+++ src/AsyncComponent.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <div style="background-color: #fff; height: 100vh;">
+    <h1 style="margin-top: 0; padding-top: 20px;">Scroll down ⬇</h1>
+    </div>
+    <div style="position: relative; z-index: 9999; background-color: #fff;">
+      <h1 style="margin:0;">Parallax Effect</h1>
+      <parallax>
+        <img src="./assets/pexels-photo-235243.jpeg">
+      </parallax>
+    </div>
+    <div style="background-color: #fff; height: 100vh;"></div>
+    <h1>Parallax fixed position </h1>
+
+    <div style="position: relative;">
+      <parallax :fixed="true">
+        <img src="./assets/pexels-photo-236118.jpeg">
+      </parallax>
+    </div>
+
+    <div style="background-color: #fff; height: 100vh;"></div>
+  </div>
+</template>
+
+<script>
+import Parallax from 'vue-parallaxy'
+
+export default {
+  components: {
+    Parallax,
+  },
+};
+</script>
+<style>
+  .test {
+    width: 99%;
+    height: 2000px;
+    background-color: white;
+    margin: 0 auto;
+  }
+  .pp {
+    background-color: blue;
+  }
+</style>
